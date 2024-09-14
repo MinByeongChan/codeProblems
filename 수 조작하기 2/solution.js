37dd24dcaa2c48506b9d49d2f8b13817b9e86426
ILikeTteokBoKki <38879025+MinByeongChan@users.noreply.github.com>
2024-09-14 18:34:43 +0900

    Create solution.js

---
+++ 수 조작하기 2/solution.js
@@ -0,0 +1,13 @@
+function solution(numLog) {
+    let answer = '';
+    
+    for(let i=1; i<numLog.length; i++) {
+        const diff = numLog[i] - numLog[i-1];
+        if(diff === 1) answer += 'w';
+        else if(diff === -1) answer += 's';
+        else if(diff === 10) answer += 'd';
+        else if(diff === -10) answer += 'a';
+    }
+    
+    return answer;
+}
